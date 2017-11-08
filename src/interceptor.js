4ad1d48b46f29a9426b1544d80c3b7f49a43bd03
Masaaki Goshima <goccy54@gmail.com>
2017-11-08 16:11:04 +0900

    add interceptor.js

---
+++ src/interceptor.js
@@ -0,0 +1,21 @@
+export default class AEDataInterceptor {
+    constructor(config) {
+        this.config = config;
+    }
+
+    add(name, param) {
+        if (!this.config) this.config = {};
+        this.config[name] = param;
+    }
+
+    intercept(data) {
+        const cfg = this.config[data.nm] || this.config[data.id];
+        if (cfg) {
+            if (cfg.text)      data.text      = cfg.text;
+            if (cfg.image)     data.image     = cfg.image;
+            if (cfg.imagePath) data.imagePath = cfg.imagePath;
+            if (cfg.events)    data.events    = cfg.events;
+        }
+        return data;
+    }
+}
