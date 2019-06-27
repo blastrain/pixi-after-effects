255247a5d63c1e4a09dc9b7e48edcc312e2525ad
Masaaki Goshima <goccy54@gmail.com>
2019-06-27 18:21:18 +0900

    fix semantivVersion interface for return value from array to object

--- src/versionHelper.js
+++ src/versionHelper.js
@@ -4,17 +4,16 @@ const pixiVersionHelper = {
   version: PIXI.VERSION,
   semanticVersion() {
     const semVer = this.version.match(/([0-9]+)\.([0-9]+)\.([0-9]+)/);
-    if (semVer.length < 3) {
-      return [0, 0, 0];
-    }
+    if (semVer.length < 3) { return { major:0, minor:0, patch:0 }; }
+
     const major = parseInt(semVer[1], 10);
     const minor = parseInt(semVer[2], 10);
     const patch = parseInt(semVer[3], 10);
-    return [major, minor, patch];
+    return { major, minor, patch };
   },
-  major() { return this.semanticVersion()[0]; },
-  minor() { return this.semanticVersion()[1]; },
-  patch() { return this.semanticVersion()[2]; },
+  major() { return this.semanticVersion().major; },
+  minor() { return this.semanticVersion().minor; },
+  patch() { return this.semanticVersion().patch; },
   isV4()  { return this.major() === 4; },
   isV5()  { return this.major() === 5; },
   select(v4func, v5func) {
