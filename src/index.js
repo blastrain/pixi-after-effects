eaf6f7b84f7971470441047dc23b2bcbbe541da2
Masaaki Goshima <goccy54@gmail.com>
2019-06-27 17:36:31 +0900

    add version helper

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as PIXI from 'pixi.js';
 import AfterEffects from './AfterEffects';
 import AEDataLoader from './loader';
 import AEDataInterceptor from './interceptor';
+import versionHelper from './versionHelper';
 
 PIXI.AfterEffects = AfterEffects;
 PIXI.AEDataLoader = AEDataLoader;
---
+++ src/versionHelper.js
@@ -0,0 +1,27 @@
+import * as PIXI from 'pixi.js';
+
+const pixiVersionHelper = {
+  version: PIXI.VERSION,
+  semanticVersion() {
+    const semVer = this.version.match(/([0-9]+)\.([0-9]+)\.([0-9]+)/);
+    if (semVer.length < 3) {
+      return [ 0, 0, 0 ];
+    }
+    const major = parseInt(semVer[1], 10);
+    const minor = parseInt(semVer[2], 10);
+    const patch = parseInt(semVer[3], 10);
+    return [ major, minor, patch ];
+  },
+  major() { return this.semanticVersion()[0]; },
+  minor() { return this.semanticVersion()[1]; },
+  patch() { return this.semanticVersion()[2]; },
+  isV4()  { return this.major() === 4; },
+  isV5()  { return this.mojor() === 5; },
+  select(v4func, v5func) {
+    if (this.isV4() && v4func !== undefined) { return v4func; };
+    if (this.isV5() && v5func !== undefined) { return v5func; };
+    return () => {};
+  }
+};
+
+export default pixiVersionHelper;
