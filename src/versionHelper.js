7d50bcf52df7bd6972c8acea4ffe21b8e2df79cc
Masaaki Goshima <goccy54@gmail.com>
2019-06-27 17:52:17 +0900

    fix lint error

--- src/versionHelper.js
+++ src/versionHelper.js
@@ -5,12 +5,12 @@ const pixiVersionHelper = {
   semanticVersion() {
     const semVer = this.version.match(/([0-9]+)\.([0-9]+)\.([0-9]+)/);
     if (semVer.length < 3) {
-      return [ 0, 0, 0 ];
+      return [0, 0, 0];
     }
     const major = parseInt(semVer[1], 10);
     const minor = parseInt(semVer[2], 10);
     const patch = parseInt(semVer[3], 10);
-    return [ major, minor, patch ];
+    return [major, minor, patch];
   },
   major() { return this.semanticVersion()[0]; },
   minor() { return this.semanticVersion()[1]; },
@@ -18,10 +18,10 @@ const pixiVersionHelper = {
   isV4()  { return this.major() === 4; },
   isV5()  { return this.mojor() === 5; },
   select(v4func, v5func) {
-    if (this.isV4() && v4func !== undefined) { return v4func; };
-    if (this.isV5() && v5func !== undefined) { return v5func; };
+    if (this.isV4() && v4func !== undefined) { return v4func; }
+    if (this.isV5() && v5func !== undefined) { return v5func; }
     return () => {};
-  }
+  },
 };
 
 export default pixiVersionHelper;
